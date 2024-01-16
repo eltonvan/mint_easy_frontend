f83c17997a38821be0b79f5ebb6f3fefe342ae9e
eltonvansoylent <eltonvansoylent@gmail.com>
2024-01-16 17:43:29 +0100

    feat: websocket without timer

--- smart_invest/src/components/portfolio/Portfolio.tsx
+++ smart_invest/src/components/portfolio/Portfolio.tsx
@@ -1,7 +1,59 @@
+import React, { useEffect } from "react";
+import protobuf from "protobufjs";
 import "./portfolio.scss";
 
-export const Portfolio = () => {
+export const Portfolio: React.FC = () => {
+  useEffect(() => {
+    // open websocket connection to Yahoo Finance
+
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+
+    
+
+
+    protobuf.load('/YPricingData.proto', (error, root) => {
+      if (error) {
+        console.error('Error loading protobuf:', error);
+        return;
+      }
+
+      const Yaticker = root.lookupType("yaticker");
+     
+      
+      
+      ws.onopen = () => {
+        console.log('connected');
+        ws.send(JSON.stringify({
+          subscribe: ['MSFT', 'AAPL']
+          
+        }));
+      };
+
+      ws.onclose = () => {
+        console.log('disconnected');
+      };
+
+
+
+      ws.onmessage = (event) => {
+        try {
+          const messageData = event.data;
+
+          // let timer = setInterval(messageData, 6000);
+          // clearInterval(timer);
+          // ws.close();
+          
+          const decodedData = Yaticker.decode(new Uint8Array(atob(messageData).split('').map(c => c.charCodeAt(0))));
+
+          console.log(decodedData);
+        } catch (decodeError) {
+          console.error('Error decoding message:', decodeError);
+        }
+      };
+    });
+  }, []); 
+
   return (
     <div className='portfolio'>Portfolio</div>
-  )
-}
+  );
+};
--- smart_invest/src/pages/trading/Trading.tsx
+++ smart_invest/src/pages/trading/Trading.tsx
@@ -3,35 +3,53 @@ import "./trading.scss";
 
 import News from "../../components/news/News";
 import UserStatus from "../../components/userStatus/UserStatus";
-
-
-import TickerPanel from "../../components/tickerPanel/TickerPanel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import ChartBox from "../../components/chartBox/ChartBox";
 import { chartBoxProduct, chartBoxUser,chartBoxConversion, chartBoxRevenue, barChartBoxVisit, barChartBoxRevenue, userStockData } from "../../data";
-// import { TextBox } from "../../components/textBox/TextBox";
+
 
 
 
 const Trading = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        arrows: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed:10000,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        adaptiveHeight: true,
+        centerMode: true,
+        centerPadding: "40px",
+        className: "slides",
+      };
 
     return (
         <div className="trading">
-  
-            <div className="box box10 
-            ">
-        <div className="chartBoxWrapper">
-          <div className="chartBoxContainer"><ChartBox {...chartBoxUser} /></div>
-          <div className="chartBoxContainer"><ChartBox {...chartBoxProduct} /></div>
-          <div className="chartBoxContainer"><ChartBox {...chartBoxConversion} /></div>
-          <div className="chartBoxContainer"><ChartBox {...chartBoxRevenue} /></div>
 
-
-        </div>
+         <div className="box box10">
+         <Slider {...settings}>
             
-
+            
+                {userStockData.map(({id, ...otherProps}) => (
+                    
+                    <div key={id} className="chartBoxContainer"><ChartBox {...otherProps} /></div>
+                ))}
+           
+    
+        
+           </Slider>
+        
             
             
             </div>
+            
 
             <div className="box box11"><UserStatus/></div>
 

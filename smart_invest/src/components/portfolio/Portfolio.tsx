d08e882cb8b534e608911a177bd7eda329569cc0
eltonvansoylent <eltonvansoylent@gmail.com>
2024-01-17 09:13:36 +0100

    feat: websocket without timer-2

--- smart_invest/src/components/portfolio/Portfolio.tsx
+++ smart_invest/src/components/portfolio/Portfolio.tsx
@@ -1,15 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import protobuf from "protobufjs";
 import "./portfolio.scss";
 
 export const Portfolio: React.FC = () => {
+  const [current, setCurrent] = useState<any>(null);
+
   useEffect(() => {
-    // open websocket connection to Yahoo Finance
-
     const ws = new WebSocket('wss://streamer.finance.yahoo.com');
-
-    
-
 
     protobuf.load('/YPricingData.proto', (error, root) => {
       if (error) {
@@ -18,14 +15,11 @@ export const Portfolio: React.FC = () => {
       }
 
       const Yaticker = root.lookupType("yaticker");
-     
-      
-      
+
       ws.onopen = () => {
         console.log('connected');
         ws.send(JSON.stringify({
           subscribe: ['MSFT', 'AAPL']
-          
         }));
       };
 
@@ -33,27 +27,33 @@ export const Portfolio: React.FC = () => {
         console.log('disconnected');
       };
 
-
-
       ws.onmessage = (event) => {
         try {
           const messageData = event.data;
-
-          // let timer = setInterval(messageData, 6000);
-          // clearInterval(timer);
-          // ws.close();
-          
-          const decodedData = Yaticker.decode(new Uint8Array(atob(messageData).split('').map(c => c.charCodeAt(0))));
-
-          console.log(decodedData);
+          const next = Yaticker.decode(new Uint8Array(atob(messageData).split('').map(c => c.charCodeAt(0))));
+          setCurrent(next);
+          console.log(next);
         } catch (decodeError) {
           console.error('Error decoding message:', decodeError);
         }
       };
     });
+
+    // Cleanup function to close the WebSocket
+    return () => {
+      ws.close();
+    };
   }, []); 
 
   return (
-    <div className='portfolio'>Portfolio</div>
+    <div className='portfolio'>
+      <h1>Portfolio</h1>
+      {current && (
+        <div>
+          <h2>Current Data:</h2>
+          <pre>{JSON.stringify(current, null, 2)}</pre>
+        </div>
+      )}
+    </div>
   );
 };

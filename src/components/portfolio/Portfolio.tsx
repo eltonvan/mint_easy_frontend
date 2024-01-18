44dab0d86f0cc70f1ffdf624c4e45d78bba7d9a3
eltonvansoylent <eltonvansoylent@gmail.com>
2024-01-18 13:45:42 +0100

    commit

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from "react";
-import protobuf from "protobufjs";
+import React, { useEffect, useState } from "react"; 
+import protobuf from "protobufjs"; // decode the binary data
 import "./portfolio.scss";
 
 export const Portfolio: React.FC = () => {
   const [current, setCurrent] = useState<any>(null);
 
-  useEffect(() => {
-    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+  useEffect(() => { 
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com'); // connect to the WebSocket
 
     protobuf.load('/YPricingData.proto', (error, root) => {
       if (error) {
         console.error('Error loading protobuf:', error);
         return;
       }
+      // get the Yaticker type from the protobuf
+      const Yaticker = root.lookupType("yaticker"); 
 
-      const Yaticker = root.lookupType("yaticker");
-
-      ws.onopen = () => {
+      // subscribe to the MSFT and AAPL tickers
+      ws.onopen = () => { 
         console.log('connected');
         ws.send(JSON.stringify({
           subscribe: ['MSFT', 'AAPL']
         }));
       };
-
+      
+      // handle the close event
       ws.onclose = () => {
         console.log('disconnected');
       };
 
+      // handle the message event
       ws.onmessage = (event) => {
         try {
           const messageData = event.data;
+          // decode the binary data ,set the current state and loop through the data
           const next = Yaticker.decode(new Uint8Array(atob(messageData).split('').map(c => c.charCodeAt(0))));
           setCurrent(next);
           console.log(next);
